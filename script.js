f53e5a07936d33b6ef0bddb4a71cd26309fb6493
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-05-20 20:00:53 -0300

    added rotate effect

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-cards = document.querySelectorAll(".card")
-cardsArray = [...cards]
+let cards = document.querySelectorAll(".card")
+let cardsArray = [...cards]
+let distribute = [0,0]
 
-    for(i=0; i< cardsArray.length; i++){
-        cardsArray[i].addEventListener("click", (e)=>{
-            e.target.parentElement.setAttribute("class", "rotate")
-            console.log(e.target.parentElement)
-        })
+
+for(i=0; i< cardsArray.length; i++){
+
+    cardsArray[i].addEventListener("click", (e)=>{
+        rotate(e)
+    })
+
+
+}
+
+ positions(cardsArray.length)
+
+
+    function positions(total){
+        pos_distribuite = 0
+        randomize = Math.round(Math.random() * total)
+        while(distribute[0] + distribute[1] == total){
+            if 
+        }
     }
 
+
+    function rotate(e){
+
+        let card = e.target.parentElement
+
+
+
+        if(card.className == "card"){
+            card.setAttribute("class", "card rotate")
+        }
+
+        else if(card.className == "card rotate" || turns == 2){
+            card.setAttribute("class","card")
+        }
+    }
