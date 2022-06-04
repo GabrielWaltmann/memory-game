7b92e9cd67b2f94ffe037e84c1dbc920652bba5e
Gabriel Waltmann <97472013+GabrielWaltmann@users.noreply.github.com>
2022-06-03 22:12:21 -0300

    second click bug fixed

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ let SRCS = ["images/cards/criper.png",
 
 let cards = []
 let cheap = document.querySelector(".cheap")
-let total = 0
+let total = 1
+checks = []
 
 function createCards(){
     for(i=0;i< 20; i++){
@@ -58,56 +59,60 @@ function click(){
     pairs = []
 
     clicks.forEach(click => click.addEventListener('click',(e)=>{
-        if(pairs[0] == pairs[1]-10 ||
-            pairs[0]-10 == pairs[1]){
-            document.getElementById(pairs[0]).className += (" blockRotate")
-            document.getElementById(pairs[1]).className += (" blockRotate")
-            console.log(document.getElementById(pairs[0]).className,document.getElementById(pairs[1]).className)
-        }
 
-        let element = e.target.parentElement
-        
-        element.classList.toggle("rotate")
-        
-        checkPair(element)
+        let element = e.target.parentElement 
+
+        if(element.className == "card rotate" ){
+            console.log(element.children[1])
+        }else if(element.className == "card" ){
+            checkPair(element)
+        } 
     }
-
         
     ))
 }
 
 function checkPair(element){
-    if (pairs[0] == null && pairs[1] == null){
-        pairs[0] = element.id
-    }else if (pairs[0] == null){
+
+    if (pairs[0] == null){
         pairs[0] = element.id
     }else if (pairs[1] == null){
         pairs[1] = element.id
-    }else if(pairs[0] == pairs[1]-10 ||
-             pairs[0]-10 == pairs[1]){
+    }else if(pairs[0] == pairs[1]-10 || pairs[0]-10 == pairs[1]){
         total++
 
+        checks.push(pairs[0],pairs[1])
+
         pairs[0] = element.id
         pairs[1] = null
-
-        checkWin()
-            
-    }else {
+    }else{
         document.getElementById(pairs[0]).classList.remove("rotate")
         document.getElementById(pairs[1]).classList.remove("rotate")
-
-
         pairs[0] = element.id
         pairs[1] = null
-        
+
+    }
+
+    rotate(element)
+
+
+}
+
+function rotate(element){
+
+    element.classList.toggle("rotate")
+
+}
+
+
+function check(){
+    if (total >= 10){
+        win()
+    }else{
+        alert(`Faltam ${10-total} pares a serem encontrados!`)
     }
 }
 
-function checkWin(){
-    if (total == 10){
-        win()
-    }     
-}
 
 function win(){
         document.body.innerHTML += `
